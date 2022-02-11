05fac21d781c2028e10893fd4a4aa8b39846759a
dialguiba <dialguiba@gmail.com>
2022-02-10 20:33:45 -0500

    add backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const estimacionesRouter = require('./routes/estimaciones')
-const realesRouter = require('./routes/reales')
+const express = require("express");
+const estimacionesRouter = require("./routes/estimaciones");
+const realesRouter = require("./routes/reales");
+var cors = require("cors");
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-app.use('/estimaciones', estimacionesRouter)
-app.use('/reales', realesRouter)
+app.use(cors());
+
+app.use("/estimaciones", estimacionesRouter);
+app.use("/reales", realesRouter);
 
 app.listen(port, () => {
-  console.log(`Aplicación funcionando en el puerto ${port}`)
-})
+  console.log(`Aplicación funcionando en el puerto ${port}`);
+});
--- backend/controllers/reales.js
+++ backend/controllers/reales.js
@@ -1,20 +1,13 @@
 const onzasMedidas = {
   sensores: [1, 1.1, 2.5, 0.8, 0.9, 0.8, 0.8],
-  fechas: [
-    "2022-01-23",
-    "2022-01-24",
-    "2022-01-25",
-    "2022-01-26",
-    "2022-01-27",
-    "2022-01-28",
-    "2022-01-29"
-  ]
-}
+  fechas: ["2022-01-23", "2022-01-24", "2022-01-25", "2022-01-26", "2022-01-27", "2022-01-28", "2022-01-29"],
+};
 
 const get = (req, res) => {
-  res.status(500).send("No implementado")
-}
+  /* res.status(500).send("No implementado"); */
+  res.status(200).send(onzasMedidas);
+};
 
 module.exports = {
-  get
-}
+  get,
+};
